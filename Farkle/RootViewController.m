37fb7f7b6e4fce25bfffb855a988ad1957175120
Andrew Liu <dieseliu321@gmail.com>
2014-10-29 21:02:48 -0700

    score

--- Farkle/RootViewController.m
+++ Farkle/RootViewController.m
@@ -15,6 +15,13 @@
 @property (strong, nonatomic) NSMutableArray *dice;
 @property (strong, nonatomic) IBOutlet UILabel *userScore;
 @property int tapCount;
+@property int oneCount;
+@property int twoCount;
+@property int threeCount;
+@property int fourCount;
+@property int fiveCount;
+@property int sixCount;
+@property int score;
 
 @end
 
@@ -27,19 +34,22 @@
     {
         label.delegate = self;
     }
-
     self.dice = [@[] mutableCopy];
     self.tapCount = 0;
+    self.oneCount = 0;
+    self.twoCount = 0;
+    self.threeCount = 0;
+    self.fourCount = 0;
+    self.fiveCount = 0;
+    self.sixCount = 0;
+    self.score = 0;
 }
 
 - (IBAction)onRollButtonPressed:(UIButton *)sender
 {
-
     if (self.tapCount == 0)
     {
-    self.dice = [NSMutableArray arrayWithArray:self.labelCollection];
-
-
+        self.dice = [NSMutableArray arrayWithArray:self.labelCollection];
         for (DieLabel *label in self.dice)
         {
             [label roll];
@@ -47,26 +57,149 @@
     }
     else
     {
-
         for (DieLabel *label in self.dice)
         {
             [label roll];
         }
     }
-
     self.tapCount++;
+
+//    for (DieLabel *label in self.dice)
+//    {
+//        if ([label.text isEqual:@"1"])
+//        {
+//            self.oneCount++;
+//        }
+//        if
+//            ([label.text isEqual:@"2"])
+//        {
+//            self.twoCount++;
+//        }
+//        if
+//            ([label.text isEqual:@"3"])
+//        {
+//            self.threeCount++;
+//        }
+//        if
+//            ([label.text isEqual:@"4"])
+//        {
+//            self.fourCount++;
+//        }
+//        if
+//            ([label.text isEqual:@"5"])
+//        {
+//            self.fiveCount++;
+//        }
+//        if
+//            ([label.text isEqual:@"6"])
+//        {
+//            self.sixCount++;
+//        }
+//        if (self.oneCount == 3)
+//        {
+//            self.userScore.text=@"100";
+//        }
+//        if (self.twoCount == 3)
+//        {
+//            self.userScore.text=@"200";
+//        }
+//        if (self.threeCount == 3)
+//        {
+//            self.userScore.text=@"300";
+//        }
+//        if (self.fourCount == 3)
+//        {
+//            self.userScore.text=@"400";
+//        }
+//        if (self.fiveCount == 3)
+//        {
+//            self.userScore.text=@"500";
+//        }
+//        if (self.sixCount == 3)
+//        {
+//            self.userScore.text=@"600";
+//        }
+//    }
 
 
 }
 
 -(void)dieTapped:(UILabel *)label
 {
-
+    if (self.tapCount == 0)
+    {
+        nil;
+    }
+    else
+    {
     label.backgroundColor = [UIColor redColor];
     [self.dice removeObject:label];
 
-}
 
+        if ([label.text isEqual:@"1"])
+        {
+            self.oneCount++;
+        }
+        if
+            ([label.text isEqual:@"2"])
+        {
+            self.twoCount++;
+        }
+        if
+            ([label.text isEqual:@"3"])
+        {
+            self.threeCount++;
+        }
+        if
+            ([label.text isEqual:@"4"])
+        {
+            self.fourCount++;
+        }
+        if
+            ([label.text isEqual:@"5"])
+        {
+            self.fiveCount++;
+        }
+        if
+            ([label.text isEqual:@"6"])
+        {
+            self.sixCount++;
+        }
+
+        if (self.oneCount == 3)
+        {
+            self.score=self.score+1000;
+            self.oneCount = 0;
+        }
+        if (self.twoCount == 3)
+        {
+            self.score=self.score+200;
+                        self.twoCount = 0;
+        }
+        if (self.threeCount == 3)
+        {
+            self.score=self.score+300;
+                        self.threeCount = 0;
+        }
+        if (self.fourCount == 3)
+        {
+            self.score=self.score+400;
+                        self.fourCount = 0;
+        }
+        if (self.fiveCount == 3)
+        {
+            self.score=self.score+500;
+                        self.fiveCount = 0;
+        }
+        if (self.sixCount == 3)
+        {
+            self.score=self.score+600;
+                        self.sixCount = 0;
+        }
+
+        self.userScore.text=[NSString stringWithFormat:@"%d", self.score];
+    }
+}
 
 
 @end
